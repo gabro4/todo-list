696279d4ba0355a07ea7b5e28d03f733661331d8
gabro4 <gabrosk@wp.pl>
2020-05-31 20:53:28 +0200

    Dodano weryfikacje danych wprowadzonych przez uzytkownika

--- client/index.js
+++ client/index.js
@@ -29,9 +29,12 @@ addTaskForm.addEventListener('submit', (event) => {
   setTimeout(() => {
     addTask()
       .then((response) => {
-        if (!response.ok) {
-          throw Error('Wystąpił błąd podczas dodawania zadania. Spróbuj ponownie później.')
-        }
+        if (response.status == 400) {
+          throw Error('Nie można dodać zadania bez tytułu. Podaj tytuł zadania i spróbuj ponownie.')
+      }
+      if (!response.ok) {
+        throw Error('Wystąpił błąd podczas dodawania zadania. Spróbuj ponownie później.')
+      }
 
         addTaskMsg.textContent = 'Pomyślnie dodano zadanie.'
         addTaskMsg.classList.add('is-success')
